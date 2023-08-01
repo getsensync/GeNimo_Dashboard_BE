96dffb26d25f2c54bb0b62f3ec667a6714544c19
GleenMike <rahmatrafid22@gmail.com>
2023-08-02 06:22:14 +0700

    [Fix] Syntax add column gender

--- migrations/sqls/20230729231708-updateCredentials-up.sql
+++ migrations/sqls/20230729231708-updateCredentials-up.sql
@@ -1,8 +1,13 @@
 /* Replace with your SQL commands */
 ---- Add the column without NOT NULL constraint and DEFAULT value
 ALTER TABLE ONLY public.credentials
-ADD gender VARCHAR(6) default 'male' CHECK (gender IN ('male', 'female'));
+ADD gender character varying(6) DEFAULT 'male'::character varying;
 --
+
+-- ADD Constraint
+ALTER TABLE ONLY public.credentials
+ADD CONSTRAINT credentials_gender_check CHECK (((gender)::text = ANY ((ARRAY['male'::character varying, 'female'::character varying])::text[])));
+
 -- Alter the column to set it as NOT NULL
 ALTER TABLE ONLY public.credentials
 ALTER COLUMN gender SET NOT NULL;
